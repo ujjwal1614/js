2d5bda772770bdc65374023a67b33356c4e3a589
ujjwal1614 <ujjwal.1614@gmail.com>
2024-10-08 11:11:24 +0000

    more about conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -43,11 +43,44 @@
 // "" => false
 // "ujjwal" => true
 
-let a = 33
+// let a = 33
+// let b = String(a)
+// console.log(b);
+// console.log(typeof b);
 
-let b = String(a)
-console.log(b);
-console.log(typeof b);
+//******************Operations***************** */
 
+// let value = 4
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "ujjwal "
+// let str2 = "bhatia"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100;
+++gamecounter;  // also postfix is same i.e gamecounter++
+console.log(gamecounter);
 
 
