987ce97b117522b527e0372ebe72d8effa090add
ujjwal1614 <ujjwal.1614@gmail.com>
2024-10-08 09:18:16 +0000

    conversion of dataType

---
+++ basics/conversion.js
@@ -0,0 +1,53 @@
+// let score = 33
+// console.log(typeof score);
+// let score2 = "abc"
+// console.log(typeof score2);
+// let score3 = null
+// console.log(typeof score3);
+// let score4 = undefined
+// console.log(typeof score4);
+// let score5 = true
+// console.log(typeof score5);
+
+// let valueInNumber = Number(score2)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3);
+// console.log(valueInNumber3);
+
+// let valueInNumber4= Number(score4)
+// console.log(typeof valueInNumber4);
+// console.log(valueInNumber4);
+
+// let valueInNumber5= Number(score5)
+// console.log(typeof valueInNumber5);
+// console.log(valueInNumber5);
+
+
+// "33" => 33
+// "abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1
+// let bolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(bolleanIsLoggedIn);
+
+// let isLoggedIn2 = ""
+// let bolleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(bolleanIsLoggedIn2);
+
+// 1 => true; 0 => false
+// "" => false
+// "ujjwal" => true
+
+let a = 33
+
+let b = String(a)
+console.log(b);
+console.log(typeof b);
+
+
+
