dab8f361e27fe46d0be2b596279b5ffff3fa9b68
ujjwal1614 <ujjwal.1614@gmail.com>
2024-10-08 08:36:12 +0000

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,37 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3+3 )  // we are using nodejs , not browser
+
+console.log(4 + 3);
+
+console.log("ujjwal")
+
+let name = "Ujjwal"
+let age = 18
+let isloggedIn = true
+let state = null
+
+
+
+// DataTypes
+
+// number => 2 to power 53
+// bigint => if number is too big]
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "ujjwal");
+console.log(typeof age);
+
+
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
---
+++ basics/variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "ujjwal@gmail.com"
+var accountPassword = "34245"
+accountCity = "jaipur"
+let accountState = ""
+
+// accountId = 756882 // not allowed
+
+accountPassword = "53444"
+accountEmail = "ujjwal16@gmail.com"
+accountCity = "indri"
+console.log(accountId);
+
+
+/*
+prefer not to use var because of issue in block scope and functional scope i.e 
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// code readability should be high
